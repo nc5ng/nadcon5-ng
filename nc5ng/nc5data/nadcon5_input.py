379ecf5f453721aca227915233bcf4d9f3802f5d
Andrey K Shmakov <akshmakov@gmail.com>
2018-02-10 20:22:59 -0800

    Added Comparison Operators  to DataPoint (<,<=,>,>=) . Added High Level Conversion class to orchestrate loading internal pieces

---
+++ nc5ng/nc5data/nadcon5_input.py
@@ -0,0 +1,111 @@
+from .nadcon5_files import ControlFileParser, InFileParser, FileBackedMetaBase, WorkEditsFileParser, SingletonFileBackedMeta, GridParamFileParser
+from .nadcon5_types import DataPoint, DataContainerMixin, MetaMixin
+import pkg_resources
+
+
+class RegionData(DataContainerMixin, MetaMixin, metaclass = SingletonFileBackedMeta, Parser = GridParamFileParser):
+    """@ingroup nc5data
+
+    """
+        
+
+    class GridBound(DataPoint):
+        def __init__(self, region, n,s,w,e):
+            super().__init__(region=region,N=n, S=s, W=w, E=e)
+                        
+
+    def __init_indexed_data__(self):
+        self._indexed_data = { region:self.GridBound(region, *data) for region, *data in self.data}
+
+
+
+
+def _extract_subregion(in_file):
+    vals = in_file.split('.')
+    if len(vals) == 4:
+        return None
+    if len(vals) == 5:
+        return vals[-2]
+
+class ControlData(DataContainerMixin, MetaMixin, metaclass=FileBackedMetaBase, Parser=ControlFileParser):
+    class ControlDataPoint(DataPoint):
+        def __init__(self, region, old_datum, new_datum, in_file):
+            
+            super().__init__( region=region, old_datum=old_datum, new_datum=new_datum, in_file = in_file, subregion=_extract_subregion(in_file))
+            
+        
+
+
+    def __init_indexed_data__(self):
+        self._indexed_data = [ ControlData.ControlDataPoint(self.region, self.source, self.target, in_file) for in_file in self.data ]
+        self._index_tag = 'subregion'
+        self._indexed_data = { point.subregion : point for point in self._indexed_data }
+        
+
+    @property
+    def region(self):
+        return self._data['meta']['REGION']
+
+    @property
+    def source(self):
+        return self._data['meta']['DATUM1']
+
+    @property
+    def target(self):
+        return self._data['meta']['DATUM2']
+    
+    @property
+    def rejmet(self):
+        return int(self._data['meta']['REJMET'])
+
+    @property
+    def n_infiles(self):
+        return int(self._data['meta']['NFILES'])
+        
+    @property
+    def infiles(self):
+        return self._data['data']
+
+            
+
+
+
+
+
+
+
+class InData(DataContainerMixin, MetaMixin,  metaclass=FileBackedMetaBase, Parser=InFileParser):
+            
+    class InDataPoint(DataPoint):
+        def __init__(self, pid, subregion, oldlat, oldlon, oldht, newlat, newlon, newht, **kwargs):
+            super().__init__(pid=pid, subregion=subregion, oldlat=oldlat, oldlon=oldht, newlat=newlat, newlon=newlon, newht=newht, **kwargs)
+           
+
+    def __init_indexed_data__(self):
+        self._index_tag = 'pid'
+        self._indexed_data = { pid:InData.InDataPoint(pid, *data) for pid, *data in self.data}
+
+
+    @property
+    def source(self):
+        if 'meta' in self._data and 'source' in self._data['meta']:
+            return self._data['meta']['source']
+
+            
+                 
+        
+            
+    
+
+
+class ExclusionData(DataContainerMixin, MetaMixin, metaclass=SingletonFileBackedMeta, Parser=WorkEditsFileParser):
+    
+    class ExclusionDataPoint(DataPoint):
+        def __init__(self,  olddtm, newdtm, region,pid,rejlat, rejlon, rejeht, reason, **kwargs):
+            super().__init__( olddtm=olddtm, newdtm=newdtm, region=region,pid=pid,rejlat=rejlat, rejlon=rejlon, rejeht=rejeht, reason=reason, **kwargs)
+
+
+    def __init_indexed_data__(self):
+        self._index_tag = 'pid'
+        self._indexed_data = { data[3]:self.ExclusionDataPoint(*data) for data in self.data }
+    
--- nc5ng/nc5data/nadcon5_types.py
+++ nc5ng/nc5data/nadcon5_types.py
@@ -2,7 +2,42 @@ import logging
 
 
 class DataPointType(type):
+    """ Metaclass for DataPoints, defines class creation and class/hierarchy member variables 
     
+    The meta-class in part, hides some of the more rote requirements of our datapoint type
+    from the actual object hierarchy
+
+    This allows some magic like run-time casting to the correct datapoint without knowing the 
+    type specifically, and a persisitent library-wide memory backed database for quick retrieval and to minimize replication
+
+    To understand the meta class, there are a number of tutorials available online, roughly speaking this class is what is used to "create" the DataPoint class, and allows us to manipulate the class without needing any implementation details. 
+
+
+    Defining a new DataPointType Hierarchy simply requires using this class as the metaclass
+
+        class NewDataPoint(metaclass=DataPointType):
+          pass
+
+    By creating a new DataPointType, the following changes will be done to the final class
+
+      - The type will have a database (dictionary) of types registered with the base class
+      - Each type and subtype will have a point container (default set) created
+      - The shorthand name will be generated from the class name
+      - Instance Creation will be overidden and allow creation of any other data type by specifting the type shorthand as an argument
+      
+
+    Class Configuration:
+
+    Each new type has some meta-configuration available
+
+    1. To override data point registration 
+       - Create a `@classmethod`  `__register__(cls, point)` to overide how a new point is registered/saved
+       - Create a class member `_point_store` to change the underlying storage type (from set)
+    2. Override shorthand name by specific '_type_shorthand' explicitly in the class
+    
+    
+    Any class that uses this type as a metaclass will be registered 
+    """
     @classmethod
     def __prepare__(metacls, name, bases, **kargs):
         """ Prepare the new class, here for completeness
@@ -13,19 +48,31 @@ class DataPointType(type):
 
     @property
     def type_shorthand(cls):
+        """ Get the class shorthand name"""
         return cls._type_shorthand
     
     @property
-    def point_set(cls):
-        return cls._point_set
+    def point_store(cls):
+        """ Return the type-specific Point Buffer"""
+        return cls._point_store
 
     @property
     def point_database(cls):
+        """ Return the Root Database of all DataPoints in this Hierarchy"""
         return cls._cbdb
     
     
     def __new__(metacls, name, bases, namespace,  **kargs):
+        """ Create a new data point type, called on class load
 
+        Creates class attributes level point set for storage
+        
+        metaclass __new__ is executed on load time for every
+        class that uses it, it is executed after __prepare__ which
+        constructs the class object
+        """
+
+    
         logging.debug("Creating Class %s"%name)
         cls = super().__new__(metacls, name, bases, namespace)
         
@@ -33,11 +80,20 @@ class DataPointType(type):
             logging.debug("Creating Data Point Database")
             cls._cbdb = dict()
 
-        cls._type_shorthand = name.lower()
-        cls._point_set = set()
+
+        # Shorthand Name
+        cls._type_shorthand =  name.lower()
+        while cls._type_shorthand in cls._cbdb:
+            cls._type_shorthand = cls._type_shorthand + "_" # in case of name conflict, add underscore
+
+        # Point Storage
+        cls._point_store = getattr(cls, '_point_store', set() )
+        
         
         logging.debug("Registering new Data Point Type %s with shorthand %s"%(name, cls._type_shorthand))
-        cls._cbdb[cls.type_shorthand]={'type':cls, 'points': cls._point_set }
+
+        
+        cls._cbdb[cls.type_shorthand]={'type':cls, 'points': cls._point_store }
         return cls
 
     
@@ -59,10 +115,12 @@ class DataPointType(type):
         super().__init__(name, bases, namespace)
 
 
-    def _register_point(cls, point):
-        cls._point_set.add(point)
+    def __register__(cls, point):
+        """ Register a point with the class buffer """
+        cls._point_store.add(point)
 
     def __call__(cls, *args,  **kw):
+        """ Create a new Point in this hierarchy"""
 
         """
         if typename is not None:
@@ -70,8 +128,22 @@ class DataPointType(type):
                 raise TypeError("Invalid Data Point Type with Shorthand %s"%typename)
             cls = cls._cbdb[typename]['type']
         """
-        point = super().__call__(*args, **kw)
-        cls._register_point(point)
+        if args and (args[0] in cls.point_database.keys()):
+            typename=args[0]
+        elif kw and ('type' in kw):
+            typename = kw['type']
+        else:
+            typename = cls.type_shorthand
+
+
+        if typename == cls.type_shorthand:
+            point = super().__call__(*args, **kw)
+            if not getattr(point, 'ephemeral', False):
+                cls.__register__(point)
+        elif typename in cls.point_database.keys():
+            point = cls.point_database[typename]['type'].__call__(*args, **kw)
+        else:
+            return None
 
         return point
 
@@ -82,16 +154,20 @@ class DataPoint(metaclass=DataPointType):
         self._data = {}
 
         # for copy
-        if len(args) == 1:
-            kwargs = args[0]._data
-
+        if args:
+            if issubclass(args[0].__class__, DataPoint):
+                kwargs = args[0]._data
         #initialize
         for k,v in kwargs.items():
             self._data[k] = v
 
+    
     def __getattr__(self, name):
         if name in self._data:
             return self._data[name]
+        else:
+            raise AttributeError()
+    
 
     @property
     def data(self):
@@ -100,17 +176,88 @@ class DataPoint(metaclass=DataPointType):
     def __getitem__(self,name):
         if name in self._data:
             return self._data[name]
+        else:
+            raise KeyError()
+    """
+    def __eq__(self, other):
+        if not issubclass(other.__class__, DataPoint) or self.type_shorthand != other.type_shorthand:
+            return False
+        else:
+            for k in self.data:
+                if not k in other.data or self.data[k] != other.data[k]:
+                    return False
+            for k in other.data:
+                if not k in self.data:
+                    return False
+        return True
+    """
 
+    def __ge__(self, other):
+        """ Superset """
+        if  not issubclass(other.__class__, DataPoint):
+            return False
+        return other.__le__(self)
+        
+        
+    def __gt__(self, other):
+        """ Strict Superset """
+        if not issubclass(other.__class__, DataPoint):
+            return False
+        return other.__lt__(self)
+        
+    def __le__(self, other):
+        """ Subset, each element in our data is contained in the other
+        """
+        if not issubclass(other.__class__, DataPoint):
+            return False
+
+        if len(self.data) > len(other.data):
+            return False
+        else:
+            for k in self.data:
+                if not k in other.data or self[k] != other[k]:
+                    return False
+        return True
+
+    def __lt__(self, other):
+        """ Strict Subset 
+        """
+        if not issubclass(other.__class__, DataPoint):
+            return False
+        
+        if len(self.data) >= len(other.data):
+            return False
+
+        ## equality requires same length, already checked equality 
+        return self.__le__(other)
+
+    
+    def __contains__(self, other):
+        if not issubclass(other.__class__, DataPoint):
+            try:
+                k,v = other
+                if not k in self.data:
+                    return False                
+                return self[k] == v
+            except (TypeError, ValueError):
+                return other in self.data
+        else:
+            for k in other.data:
+                if not k in self.data:
+                    return False
+        return True
 
     def __repr__(self):
-        return "DataPoint(**"+repr(self._data)+")"
+        return "DataPoint('" + self.__class__.type_shorthand + "', **"+repr(self._data)+")"
 
     def __str__(self):
-        return "Data Point: " + str(self._data)
+        return "Data Point('" + self.__class__.type_shorthand + "') : " + str(self._data)
+    
+    def __bool__(self):
+        return self.data.__bool__()
     
 
-
-
+    
 
 def _safe_meta_property(index):
     def getter(self):
@@ -120,9 +267,7 @@ def _safe_meta_property(index):
             return None
     return property(getter)
     
-class MetaMixin(object):
-    
-    
+class MetaMixin(object):    
     @property
     def meta(self):
         if hasattr(self, '_data'):
@@ -140,7 +285,6 @@ class MetaMixin(object):
     
 
 class DataContainerMixin(object):
-
     @property
     def data(self):
         if hasattr(self, '_data'):
@@ -181,7 +325,6 @@ class DataContainerMixin(object):
             return self._indexed_data[index]
         else:
             return None
-
 
     def __init__(self, *args, **kwargs):
         self._data = self.__class__.parser.fromfile(*args, **kwargs)
