b7cfb8d4b6ae2d70afc642753b9d5e6c6b2ac9cc
Andrey K Shmakov <akshmakov@gmail.com>
2018-03-29 01:35:20 -0700

    Started GMT-python integration into nc5ng.nc5data

---
+++ nc5ng/nc5data/__init__.py
@@ -0,0 +1,12 @@
+from .conversion import Conversion
+from .nadcon5_input import RegionData, ControlData, InData, ExclusionData
+from .nadcon5_output import VectorData, PointData, GRDData, BData,XYZData
+from .nadcon5_types import DataPoint
+
+
+
+
+
+
+__all__ = ['Conversion', 'RegionData', 'ControlData', 'InData', 'ExclusionData',
+           'VectorData', 'PointData', 'GRDData', 'BData', 'XYZData', 'DataPoint']
